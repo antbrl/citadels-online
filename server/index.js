445b2bb7b3f458116c1359120b4c4a739b7e8e94
Antoine Brûlé <antoine.brule@netc.eu>
2021-03-09 16:32:54 +0100

    feat: add socket connection

--- server/index.js
+++ server/index.js
@@ -3,9 +3,18 @@ const app = express()
 const http = require('http').createServer(app);
 const port = 8081
 
+const io = require('socket.io')(http, { path: '/s/' });
+
 app.use(express.static(__dirname + '/static'));
 
 app.get('/', (req, res) => {
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
 http.listen(port, () => {

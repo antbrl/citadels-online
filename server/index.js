ed817b43037a965eaa7d45dc2f1c54bbe169115d
Antoine Brûlé <antoine.brule@netc.eu>
2021-03-10 19:46:59 +0100

    feat: add basic connection/disconnection

--- server/index.js
+++ server/index.js
@@ -4,16 +4,43 @@ const http = require('http').createServer(app);
 const port = 8081
 
 const io = require('socket.io')(http, { path: '/s/' });
+const players = {};
 
 app.use(express.static(__dirname + '/static'));
 
 app.get('/', (req, res) => {
 });
 
+io.use((socket, next) => {
+  const username = socket.handshake.auth.username;
+  if (!username) {
+    return next(new Error("invalid username"));
+  }
+  socket.username = username;
+  next();
+});
+
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  const player = {
+    id: socket.id,
+    name: socket.username
+  };
+  console.log(`user '${player.name}' connected [${player.id}]`);
+
+  // register player
+  players[socket.id] = player;
+  // send the players object to the new player
+  socket.emit('currentPlayers', players);
+  // update all other players of the new player
+  socket.broadcast.emit('newPlayer', players[socket.id]);
+
   socket.on('disconnect', () => {
-    console.log('user disconnected');
+    const player = players[socket.id];
+    console.log(`user '${player.name}' disconnected [${player.id}]`);
+    delete players[socket.id];
+
+    // emit a message to all players to remove this player
+    io.emit('disconnectPlayer', socket.id);
   });
 });
 

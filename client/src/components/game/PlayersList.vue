2d484fe8f7513a391ff57707e38c10e899c3a20d
Antoine Brûlé <antoine.brule@netc.eu>
2021-03-22 15:16:45 +0100

    feat(PlayersList): show self first

--- client/src/components/game/PlayersList.vue
+++ client/src/components/game/PlayersList.vue
@@ -4,14 +4,16 @@
     Players
   </div>
   <ul class="list-group list-group-flush text-dark">
+    <li class="list-group-item">
+        {{ self.username }} <span class="badge badge-info">You</span>
+    </li>
     <li
       class="list-group-item"
-      v-for="(player) in players"
+      v-for="(player) in otherPlayers"
       :key="player.id"
     >
         {{ player.username }}
-        <span v-if="player.id === self" class="badge badge-info">You</span>
-        <span v-else-if="player.online" class="badge badge-success">Online</span>
+        <span v-if="player.online" class="badge badge-success">Online</span>
         <span v-else class="badge badge-secondary">Offline</span>
     </li>
   </ul>
@@ -28,11 +30,12 @@ export default defineComponent({
     ...mapGetters([
       'gameState',
     ]),
-    players() {
-      return Array.from(this.gameState.players.values());
+    otherPlayers() {
+      return Array.from(this.gameState.players.values())
+        .filter((player) => player.id !== this.gameState.self);
     },
     self() {
-      return this.gameState.self;
+      return this.gameState.players.get(this.gameState.self);
     },
   },
 });

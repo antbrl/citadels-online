aaa009cb03cc885c9c32d4d4fa1bc0e2a4d69431
Antoine Brûlé <antoine.brule@netc.eu>
2021-04-03 13:46:23 +0200

    feat: add locale selector

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,16 @@
 <template>
-<h1>{{ $t('ui.title') }}</h1>
+<header>
+  <div class="container-lg">
+    <div class="row align-items-center">
+      <div class="col">
+        <h1><a href="/" class="text-reset">{{ $t('ui.title') }}</a></h1>
+      </div>
+      <div class="col-2 align-bottom">
+        <LocaleSelector class="opacity-4" />
+      </div>
+    </div>
+  </div>
+</header>
 <div class="body">
   <router-view></router-view>
 </div>
@@ -7,15 +18,18 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import LocaleSelector from './components/LocaleSelector.vue';
 
 export default defineComponent({
   name: 'App',
+  components: {
+    LocaleSelector,
+  },
 });
 </script>
 
 <style scoped>
-h1 {
-  text-align: center;
+header {
   background: brown;
   color: wheat;
   margin: 0;
---
+++ client/src/components/LocaleSelector.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="input-group">
+  <div class="input-group-prepend">
+    <label class="input-group-text px-2" for="inputGroupLocale">
+      <img
+        :src="`/flags/${$t('flag', $i18n.locale)}.svg`"
+        :alt="$i18n.locale"
+      >
+    </label>
+  </div>
+  <select class="custom-select" v-model="$i18n.locale" id="inputGroupLocale">
+    <option v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale">
+      {{ $t('name', locale, locale) }}
+    </option>
+  </select>
+</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'LocaleSelector',
+});
+</script>
+
+<style lang="scss" scoped>
+img {
+  height: 1em;
+}
+</style>

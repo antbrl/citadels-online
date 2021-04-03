06af98c5c872712be456777f8415f8e479a51ee3
Antoine Brûlé <antoine.brule@netc.eu>
2021-04-03 14:31:28 +0200

    feat(header): add subtitles

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,10 +2,13 @@
 <header>
   <div class="container-lg">
     <div class="row align-items-center">
-      <div class="col">
+      <div class="col-2"></div>
+      <div class="col text-center">
+        <h6>{{ $t('ui.subtitle1') }}</h6>
         <h1><a href="/" class="text-reset">{{ $t('ui.title') }}</a></h1>
+        <h6>{{ $t('ui.subtitle2') }}</h6>
       </div>
-      <div class="col-2 align-bottom">
+      <div class="col-2">
         <LocaleSelector class="opacity-4" />
       </div>
     </div>
@@ -28,12 +31,17 @@ export default defineComponent({
 });
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 header {
   background: brown;
   color: wheat;
   margin: 0;
   padding: 0.5em;
+
+  h1, h6 {
+    padding: 0;
+    margin: 0;
+  }
 }
 
 .body {

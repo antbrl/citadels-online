69c2aa7e185f350c66c4ae22094420ec14065adb
Antoine Brûlé <antoine.brule@netc.eu>
2021-04-15 22:39:25 +0200

    feat(PlayerHand): add stash coins

--- client/src/components/game/elements/PlayerHand.vue
+++ client/src/components/game/elements/PlayerHand.vue
@@ -1,13 +1,14 @@
 <template>
 <div class="pt-2 d-flex justify-content-start overflow-auto">
-  <div v-if="hand.length === 0" style="height: 9em;"></div>
   <DistrictCard
-    v-else
     v-for="id, i in hand"
     :key="i"
     :district-id="id"
     class="mr-2"
   />
+  <div
+    class="stash d-flex flex-column-reverse flex-wrap-reverse justify-content-start ml-auto"
+  ><span v-for="i in stash" :key="i" class="coin">🪙</span></div>
 </div>
 </template>
 
@@ -22,8 +23,27 @@ export default defineComponent({
   },
   props: {
     hand: {
+      type: Array,
+      required: true,
+    },
+    stash: {
+      type: Number,
       required: true,
     },
   },
 });
 </script>
+
+<style lang="scss" scoped>
+.stash {
+  height: 9em;
+}
+
+.coin {
+  font-size: 2em;
+  height: .8em;
+  position: relative;
+  bottom: .65em;
+  text-shadow: 0 -1px 2px rgba(0, 0, 0, .2);
+}
+</style>
